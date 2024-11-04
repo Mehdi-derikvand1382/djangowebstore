7812067db13504b6483803c0913d45cba46482df
mehdi derikvand <yonotomakarde@gmail.com>
2024-11-04 18:17:21 +0330

    ok

--- weblog/models.py
+++ weblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Weblog(models.Model):
+    weblog = models.CharField(max_length=225)
+
+    def __str__(self) -> str:
+        return self.weblog
--- weblog/views.py
+++ weblog/views.py
@@ -1,9 +1,9 @@
 #from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.views import View
-
+from .models import Weblog
 from django.views.generic import TemplateView
-
+from django.views.generic import ListView
 #def weblog(request):
 #    return render(request, 'home.html')
 
@@ -11,3 +11,6 @@ class WeblogView(TemplateView):
     template_name='home.html'
     #def get(self, request):
     #    return render(request, 'home.html')
+class WeblogView(ListView):
+    model =Weblog
+    template_name='home.html'
